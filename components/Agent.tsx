1eec239add58f86bb26623cc275fdb0b4a9341e2
Satwik Kumar <satwik914@icloud.com>
2025-04-19 20:38:20 +0530

    updated photo female ai avatar without bg

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -153,7 +153,7 @@ const Agent = ({
         <div className="card-interviewer">
           <div className="avatar relative w-[60%] aspect-square rounded-lg overflow-hidden mx-auto">
             <Image
-              src="/ai-avatar-sq.png"
+              src="/ai-avatar-female.png"
               alt="profile-image"
               fill
               className="object-cover"
